48bf92c6455992e56d41359e0a4172e1c699567b
ernberna <ernesto.bernabe@digitas.com>
2021-07-08 18:43:42 -0600

    first comit

---
+++ src/async/challenge.js
@@ -0,0 +1,23 @@
+const fetchData = require('../utils/fetchData');
+const API = 'https://rickandmortyapi.com/api/character/';
+
+
+const anotherFunction = async (urlApi) => {
+    try {
+        const data = await fetchData(urlApi);
+        const character = await fetchData(`${urlApi}${data.results[0].id}`);
+        const origin = await fetchData(character.origin.url);
+
+        console.log(data.info.count);
+        console.log(character.name);
+        console.log(origin.dimension);
+
+    } catch (error) {
+        console.error(error);
+    }
+
+}
+
+console.log('before');
+anotherFunction(API);
+console.log('after');
